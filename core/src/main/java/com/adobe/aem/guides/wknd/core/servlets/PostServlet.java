e17cdaa5d1085f9483af486477ff53b8d437ec4a
Alextwit <alextwit31@gmail.com>
2022-07-12 01:11:16 +0300

    Add post servlet for ajax request from browser

--- core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/PostServlet.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/PostServlet.java
@@ -1,16 +1,17 @@
 package com.adobe.aem.guides.wknd.core.servlets;
 
 import com.adobe.aem.guides.wknd.core.constants.CommonConstants;
+import com.adobe.aem.guides.wknd.core.services.ServiceResolver;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 import com.day.crx.JcrConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.*;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.servlets.annotations.SlingServletPaths;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +20,8 @@ import javax.jcr.RepositoryException;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component(service = Servlet.class)
 @SlingServletPaths(
@@ -28,19 +31,25 @@ public class PostServlet extends SlingAllMethodsServlet {
 
     public static final Logger LOG = LoggerFactory.getLogger(PostServlet.class);
 
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
+
+    @Reference
+    private ServiceResolver serviceResolver;
+
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
-        /*ResourceResolver rr = request.getResourceResolver();
-        // String path = String.format("%s/%s", request.getRequestPathInfo().getResourcePath(), JcrConstants.JCR_CONTENT);
-        Resource resource = rr.getResource(request.getRequestPathInfo().getResourcePath() + CommonConstants.SLASH + JcrConstants.JCR_CONTENT);
-        //Resource resource = request.getResource();
-        Node node = resource.adaptTo(Node.class);
+        String requestPath = request.getParameter("path");
+        String pathWithoutExtension = requestPath.substring(0, requestPath.lastIndexOf("."));
         try {
-            node.setProperty("jcr:title", "privet from post servlet");
-        } catch (RepositoryException e) {
+            ResourceResolver rr = serviceResolver.getServiceResourceResolver();
+            Resource resource = rr.getResource(pathWithoutExtension + CommonConstants.SLASH + JcrConstants.JCR_CONTENT);
+            Node node = resource.adaptTo(Node.class);
+            node.setProperty("jcr:title", "last chance to change");
+            rr.commit();
+        } catch (LoginException | RepositoryException e) {
             throw new RuntimeException(e);
         }
-        rr.commit();*/
 
 
     }
